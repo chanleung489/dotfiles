5f804b0b9ea2b4374e26bbd102821cc4d3619fd1
lchan <110078497+chanleung489@users.noreply.github.com>
2024-11-03 17:18:32 +0800

    fix nvim qf list nav keybind, include bg in repo

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -29,10 +29,10 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- quick fix action list
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- replace all instance in buffer
 vim.keymap.set("n", "<leader>ips", function()

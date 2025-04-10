f93dd552d372b1741a8a3b2fe86dae7c8aebf80c
lchan <110078497+chanleung489@users.noreply.github.com>
2025-04-10 10:13:52 +0800

    nvim wrap add json

--- nvim/.config/nvim/lua/config/autowrap.lua
+++ nvim/.config/nvim/lua/config/autowrap.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd(
         "BufEnter",
     },
     {
-        pattern = "*.txt,*.md",
+        pattern = "*.txt,*.md,*.json",
         callback = function()
             vim.opt_local.wrap = true
             vim.opt_local.linebreak = true
